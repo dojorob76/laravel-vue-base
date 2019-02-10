bb5c7ed201c10c185f8e027bfa9ad75d6723301a
Donna Robinson <donna@screenpresence.co>
2019-02-10 00:31:06 +0000

    NPM Packages and Foundational Updates

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,17 +1,15 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
+    <section class="section">
+        <div class="container">
+            <h1 class="title">
+                Hello World
+            </h1>
+            <p class="subtitle">
+                My first website with <strong>Bulma</strong>!
+            </p>
+            <p>If there is a "thumbs-up" icon here -> <i class="fa fa-thumbs-up"></i> <-, then all is working well!</p>
         </div>
-    </div>
+    </section>
 </template>
 
 <script>
